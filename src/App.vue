6157dd57a6f5369917b0508bfc0f05fd37042c66
AJ <a.anjolaoluwa@yahoo.com>
2023-01-13 13:10:21 -0800

    updated README and app for Dev

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <Header title="Stage Anime Quoter"/>
+    <Header title="Dev Anime Quoter"/>
     <Quote :quote="quote"/>
     <div class="button-container">
       <button @click="getQuote">Generate</button>
